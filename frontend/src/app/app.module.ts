753fc868e0c63b714375eeb30cc6759e036d5134
KristinaAndrijin <96635332+KristinaAndrijin@users.noreply.github.com>
2023-06-23 05:56:43 +0200

    global try

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -18,6 +18,7 @@ import { CognitoUserPool, CognitoUserAttribute } from 'amazon-cognito-identity-j
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { CognitoUserPool,CognitoUserAttribute } from 'amazon-cognito-identity-js';
-import { NgForm } from '@angular/forms';
+import { CognitoUserPool,CognitoUserAttribute  } from 'amazon-cognito-identity-js';
+import { NgForm, FormGroup, FormControl, Validators, AbstractControl} from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-registration',
@@ -10,58 +11,76 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent {
-//   isLoading:boolean = false;
-//   fname:string = '';
-//   lname:string = '';
-//   username:string = '';
-//   email:string = '';
-//   mobileNo:string = '';
-//   password:string = '';
-//   birthdate:string='';
+  isLoading:boolean = false;
+  fname:string = '';
+  lname:string = '';
+  username:string = '';
+  email:string = '';
+  mobileNo:string = '';
+  password:string = '';
+  birthdate:string='';
+  registerForm!: FormGroup;
 
-//   constructor(private router: Router) { }
 
-//   ngOnInit(): void {}
+  constructor(private router: Router) { }
 
-//   onSignup(form: NgForm){
-//     if (form.valid) {
-//      this.isLoading = true;
-//      var poolData = {
-//        UserPoolId: environment.cognitoUserPoolId, // Your user pool id here
-//        ClientId: environment.cognitoAppClientId // Your client id here
-//      };
-//      var userPool = new CognitoUserPool(poolData);
-//      var attributeList = [];
-//      let formData:formDataInterface = {
-//        "given_name": this.fname,
-//        "family_name": this.lname,
-//        "custom:username": this.username,
-//        "email": this.email,
-//        "phone_number": this.mobileNo,
-//        "birthdate": this.birthdate,
-//      }
+  ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      username: new FormControl('', Validators.required),
+      fname: new FormControl('', Validators.required),
+      lname: new FormControl('', Validators.required),
+      mobileNo: new FormControl('', Validators.required),
+      birthdate: new FormControl('', Validators.required),
+      btn: new FormControl("")},
+      // { validators: this.check },
+    );
+  }
 
-//      for (let key  in formData) {
-//        let attrData = {
-//          Name: key,
-//          Value: formData[key]
-//        }
-//        let attribute = new CognitoUserAttribute(attrData);
-//        attributeList.push(attribute)
-//      }
-//      userPool.signUp(this.email, this.password, attributeList, [], (
-//        err,
-//        result
-//      ) => {
-//        this.isLoading = false;
-//        if (err) {
-//          alert(err.message || JSON.stringify(err));
-//          return;
-//        }
-//        this.router.navigate(['/signin']);
-//      });
-//     }
-//  }
+  // onSignup() {}
+
+  onSignup() {
+    console.log("hej");
+      this.isLoading = true;
+      var poolData = {
+        UserPoolId: environment.cognitoUserPoolId, 
+        ClientId: environment.cognitoAppClientId 
+      };
+      var userPool = new CognitoUserPool(poolData); 
+      var attributeList = [];
+      let formData: formDataInterface = {
+        "given_name": this.fname,
+        "family_name": this.lname,
+        "custom:username": this.username,
+        "email": this.email,
+        "phone_number": this.mobileNo,
+        "birthdate": this.birthdate,
+      }
+  
+      for (let key in formData) {
+        let attrData = {
+          Name: key,
+          Value: formData[key]
+        }
+        let attribute = new CognitoUserAttribute(attrData); 
+        attributeList.push(attribute)
+      }
+      userPool.signUp(this.email, this.password, attributeList, [], (
+        err: any,
+        result: any
+      ) => {
+        this.isLoading = false;
+        if (err) {
+          alert(err.message || JSON.stringify(err));
+          return;
+        }
+        console.log("jej");
+        this.router.navigate(['/login']);
+      });
+  }
+  
+
 }
 
 
