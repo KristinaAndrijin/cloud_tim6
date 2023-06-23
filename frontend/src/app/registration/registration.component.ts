f0ff190897241e109b4c8d4ef2f2ef9fae21fbe4
KristinaAndrijin <96635332+KristinaAndrijin@users.noreply.github.com>
2023-06-23 08:08:00 +0200

    cognito registration works!

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CognitoUserPool,CognitoUserAttribute  } from 'amazon-cognito-identity-js';
+import { CognitoUserPool,CognitoUserAttribute, CognitoUser  } from 'amazon-cognito-identity-js';
 import { NgForm, FormGroup, FormControl, Validators, AbstractControl} from '@angular/forms';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
@@ -12,30 +12,33 @@ import { environment } from 'src/environments/environment';
 })
 export class RegistrationComponent {
   isLoading:boolean = false;
-  fname:string = '';
-  lname:string = '';
   username:string = '';
-  email:string = '';
-  mobileNo:string = '';
-  password:string = '';
-  birthdate:string='';
   registerForm!: FormGroup;
-
+  codeSent: boolean = false;
+  codeForm!: FormGroup;
+  userPool!: any;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.registerForm = new FormGroup({
-      email: new FormControl('', Validators.required),
-      password: new FormControl('', Validators.required),
-      username: new FormControl('', Validators.required),
-      fname: new FormControl('', Validators.required),
-      lname: new FormControl('', Validators.required),
-      mobileNo: new FormControl('', Validators.required),
-      birthdate: new FormControl('', Validators.required),
-      btn: new FormControl("")},
-      // { validators: this.check },
-    );
+    // if (!this.codeSent) {
+      this.registerForm = new FormGroup({
+        email: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required),
+        username: new FormControl('', Validators.required),
+        fname: new FormControl('', Validators.required),
+        lname: new FormControl('', Validators.required),
+        mobileNo: new FormControl('', Validators.required),
+        birthdate: new FormControl('', Validators.required),
+        btn: new FormControl("")},
+        // { validators: this.check },
+      );
+    // } else {
+      this.codeForm = new FormGroup({
+        code: new FormControl('', Validators.required),
+        btn: new FormControl("")},
+      );
+    // }
   }
 
   // onSignup() {}
@@ -47,15 +50,15 @@ export class RegistrationComponent {
         UserPoolId: environment.cognitoUserPoolId, 
         ClientId: environment.cognitoAppClientId 
       };
-      var userPool = new CognitoUserPool(poolData); 
+      this.userPool = new CognitoUserPool(poolData); 
       var attributeList = [];
       let formData: formDataInterface = {
-        "given_name": this.fname,
-        "family_name": this.lname,
-        "custom:username": this.username,
-        "email": this.email,
-        "phone_number": this.mobileNo,
-        "birthdate": this.birthdate,
+        "given_name": this.registerForm.get('fname')?.value,
+        "family_name": this.registerForm.get('lname')?.value,
+        "custom:username": this.registerForm.get('username')?.value,
+        "email": this.registerForm.get('email')?.value,
+        "phone_number": this.registerForm.get('mobileNo')?.value,
+        "birthdate": this.registerForm.get('birthdate')?.value,
       }
   
       for (let key in formData) {
@@ -63,10 +66,12 @@ export class RegistrationComponent {
           Name: key,
           Value: formData[key]
         }
+        // console.log(key + " : " + formData[key]);
         let attribute = new CognitoUserAttribute(attrData); 
         attributeList.push(attribute)
       }
-      userPool.signUp(this.email, this.password, attributeList, [], (
+      this.username = this.registerForm.get('username')?.value
+      this.userPool.signUp(this.username, this.registerForm.get('password')?.value, attributeList, [], (
         err: any,
         result: any
       ) => {
@@ -76,8 +81,53 @@ export class RegistrationComponent {
           return;
         }
         console.log("jej");
-        this.router.navigate(['/login']);
+        this.codeSent = true;
+      //   const username = this.registerForm.get('username')?.value;
+      // const userData = {
+      //   Username: username,
+      //   Pool: userPool
+      // };
+      // const cognitoUser = new CognitoUser(userData);
+      // console.log(cognitoUser);
+      // cognitoUser.confirmRegistration()
+        // this.router.navigate(['/login']);
       });
+
+      // const username = this.registerForm.get('username')?.value;
+      // const userData = {
+      //   Username: username,
+      //   Pool: userPool
+      // };
+      // const cognitoUser = new CognitoUser(userData);
+      // console.log(cognitoUser);
+  }
+
+  checkCode() {
+    const username = this.username;
+      const userData = {
+        Username: username,
+        Pool: this.userPool
+      };
+      const cognitoUser = new CognitoUser(userData);
+      console.log(cognitoUser);
+      let codeSuccess: boolean = false;
+      cognitoUser.confirmRegistration(this.codeForm.get('code')?.value, true, function(err, result) {
+        if (err) {
+          console.log(err);
+          alert(err.message || JSON.stringify(err));
+          // Handle the error, e.g., display an error message to the user
+        } else {
+          console.log(result);
+          alert("Slay!");
+          codeSuccess = true;
+          // this.router.navigate(['/login']);
+          // The user has been successfully confirmed
+          // You can now allow the user to sign in or perform other actions
+        }
+      })
+    if (codeSuccess) {
+      this.router.navigate(['/login']);
+    }
   }
   
 
