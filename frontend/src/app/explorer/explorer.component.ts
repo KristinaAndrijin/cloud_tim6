8a5e6f6073c7bf4ab2bc51271cc8a87cdc057470
VladimirGolosin <96448683+VladimirGolosin@users.noreply.github.com>
2023-06-27 00:52:54 +0200

    prekopirao main u explorer

--- frontend/src/app/explorer/explorer.component.ts
+++ frontend/src/app/explorer/explorer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FilesService } from '../backend_services/files.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-explorer',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./explorer.component.css']
 })
 export class ExplorerComponent {
+
+  albums: any[] = [];
+
+  constructor(private filesService: FilesService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.albums = this.filesService.getAlbums();
+  }
+
+  logClickedItem(albumName: string) {
+    console.log("Clicked item: " + albumName);
+    this.router.navigate(["explorer"]);
+  }
 
 }
