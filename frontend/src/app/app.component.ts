a013bf511074aa5692ae1f0c85e99d1f701d8423
VladimirGolosin <96448683+VladimirGolosin@users.noreply.github.com>
2023-06-27 17:44:57 +0200

    dodat navbar u invite

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -26,7 +26,7 @@ export class AppComponent {
         // Gledamo na kojoj smo putanji i prikazujemo navbar u skladu sa tim
         const navigationEndEvent = event as NavigationEnd;
         const url = navigationEndEvent.url;
-        if (url.includes('main') || url.includes('explorer') || url.includes('details') || url.includes('upload')) {
+        if (url.includes('main') || url.includes('explorer') || url.includes('details') || url.includes('upload') || url.includes("invite")) {
           this.showNavbar = true;
         } else {
           this.showNavbar = false;
