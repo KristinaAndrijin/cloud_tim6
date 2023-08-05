79141b3630522d3cea0891b08b6a9e871dbd33da
stefanium1389 <83018844+stefanium1389@users.noreply.github.com>
2023-08-05 13:16:56 +0200

    и снова в атаку себя мы бросаем
    ludilo

--- backend/aws-tim6/files/sleeper_agent.py
+++ backend/aws-tim6/files/sleeper_agent.py
@@ -1,26 +1,46 @@
 import json
+import logging
+import sys
+import traceback
+
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
-metadata_table = dynamodb.table('filesMetadata')
+table = dynamodb.Table('filesMetadata')
 sqs = boto3.client('sqs')
 queue_url = 'https://sqs.eu-central-1.amazonaws.com/275505252693/metadata-queue'
 
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 def sleeper_agent(event, context):
     try:
-
+        object_key = event["Records"][0]["s3"]["object"]["key"]
+        logger.info(f'object_key: {object_key}')
         response = sqs.receive_message(
             QueueUrl=queue_url,
-            MaxNumberOfMessages=1
+            MaxNumberOfMessages=10
         )
+        for message in response["Messages"]:
+            item_str = message['Body']
+            logger.info(f'item_json: {item_str}')
+            item = json.loads(item_str)
+            if(object_key == item["object_key"]):
+                table.put_item(Item=item)
+                sqs.delete_message(
+                    QueueUrl=queue_url,
+                    ReceiptHandle=message["ReceiptHandle"]
+                )
+                logger.info('deleted message?')
 
-        item_json = json.loads(response[0])
 
-        metadata_table.put_item(Item=item_json)
+    except Exception as exp:
+        exception_type, exception_value, exception_traceback = sys.exc_info()
+        traceback_string = traceback.format_exception(exception_type, exception_value, exception_traceback)
+        err_msg = json.dumps({
+        "errorType": exception_type.__name__,
+        "errorMessage": str(exception_value),
+        "stackTrace": traceback_string})
 
-    except Exception as e:
-
-        body = {
-            "message": str(e),
-        }
-        return {"statusCode": 500, "body": json.dumps(body)}
+        logger.error(err_msg)
