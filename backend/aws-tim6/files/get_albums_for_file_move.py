c03cafe67c379a09941ffd24412b09f7f450a281
VladimirGolosin <96448683+VladimirGolosin@users.noreply.github.com>
2023-08-25 20:16:21 +0200

    add to album dialog radi

--- backend/aws-tim6/files/get_albums_for_file_move.py
+++ backend/aws-tim6/files/get_albums_for_file_move.py
@@ -20,33 +20,47 @@ def get_albums(event, context):
                 "body": json.dumps(body)
             }
 
+        #dobavi sve iz user album, sačuvaj samo one koji počinju sa username + username sam
+        #procesom eliminacije skidaj sve one koji imaju object_key par 
         # Scan the entire DynamoDB table
-        response = dynamodb.scan(
+        response_album_object = dynamodb.scan(
             TableName='albumObject',
         )
 
-        # Filter the results to find albums without the specified obj_key_from_front
+        response_user_album = dynamodb.scan(
+            TableName='userAlbum',
+        )
+
         albums_without_object = set()
-        print(response["Items"])
-        for item in response['Items']:
+        for item in response_user_album["Items"]:
+            album_key = item['album_key']['S']
+            username_db = item['username']['S']
+
+            if album_key.split("/")[0] == username:
+                albums_without_object.add(album_key)
+
+        albums_without_object.add(username)
+
+        print(response_album_object["Items"])
+        for item in response_album_object['Items']:
             album_key = item['album_key']['S']
             object_key = item['object_key']['S']
 
-            print("album_key: "+album_key,"username:"+username, "starts:"+str(album_key.startswith(username)))
-            print("object_key: "+object_key + " obj_front: "+obj_key_from_front + " object_key != obj_key_from_front:" +str(object_key != obj_key_from_front))
-            print("ceo if: "+ str(album_key.startswith(username) and object_key != obj_key_from_front))
-            if album_key.startswith(username + '/') and object_key != obj_key_from_front:
-                print("našaooo")
-                albums_without_object.add(album_key)
+            # print("album_key: "+album_key,"username:"+username, "starts:"+str(album_key.startswith(username)))
+            # print("object_key: "+object_key + " obj_front: "+obj_key_from_front + " object_key != obj_key_from_front:" +str(object_key != obj_key_from_front))
+            # print("ceo if: "+ str(album_key.startswith(username) and object_key != obj_key_from_front))
+            if album_key.split("/")[0] == username and object_key == obj_key_from_front:
+                print("izbacujeeem")
+                if album_key in albums_without_object:
+                    albums_without_object.remove(album_key)
 
         # Prepare a list of objects with album_name and owner
         result_list = []
         print(albums_without_object)
         for album in albums_without_object:
-            album_name = album.split('/')[1]
 
             result_list.append({
-                "album_name": album_name,
+                "name": album,
                 "owner": username
             })
 
